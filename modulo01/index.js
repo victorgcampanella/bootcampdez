57fce9fac592613c786651cde1ad232bdffe2064
victorgcampanella <vgcgalvao@outlook.com>
2020-01-27 20:30:52 -0300

    trabalhando get, post,put e delete

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,11 +2,19 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+
 //Query params = ?teste=1
 //Route params = /users/1
 //Request body = {"name" : "Diego"}
 
-const users = ['Diego', 'Claudio', 'Victor']
+//CRUD = Create, Read, Update, Delete
+
+const users = ['Diego', 'Robson', 'Victor']
+
+server.get('/users', (request, response) => {
+  return response.json(users)
+})
 
 server.get('/users/:index', (request, response) => {
   const nome = request.query.nome
@@ -14,6 +22,31 @@ server.get('/users/:index', (request, response) => {
   const {index} = request.params
 
   return response.json(users[index])
+})
+
+server.post('/users', (request, response) => {
+  const {name} = request.body
+
+  users.push(name)
+
+  return response.json(users)
+})
+
+server.put('/users/:index', (request, response) => {
+  const {index} = request.params
+  const {name} = request.body
+
+  users[index] = name
+
+  return response.json(users)
+})
+
+server.delete('/users/:index', (request, response) => {
+  const {index} = request.params
+
+  users.splice(index, 1)
+
+  return response.json(users)
 })
 
 server.listen(3000)
