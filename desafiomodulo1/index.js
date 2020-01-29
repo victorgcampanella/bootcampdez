a38c5b7fc7b8f8689200102327dd454e6e67b5d6
victorgcampanella <vgcgalvao@outlook.com>
2020-01-29 15:04:04 -0300

    adicionando delete e post de tasks

--- desafiomodulo1/index.js
+++ desafiomodulo1/index.js
@@ -4,22 +4,20 @@ const server = express()
 
 server.use(express.json())
 
-const projects = [{id : '1', 
-                  title : 'Novo Projeto', 
-                  tasks: ['Fazer código', 'Fazer tela']
-                  },
-                  {id : '2', 
-                  title : 'Novo Prick', 
-                  tasks: ['Fazer botao', 'Fazer cadastro']
-                  },
-                ]
-
-server.get('/projects', (request, response) => {
-  return response.json(projects)
-})
+const projects = [{
+  id: '1',
+  title: 'Novo Projeto',
+  tasks: ['Fazer código', 'Fazer tela']
+},
+{
+  id: '2',
+  title: 'Novo Prick',
+  tasks: ['Fazer botao', 'Fazer cadastro']
+},
+];
 
 server.post('/projects', (request, response) => {
-  const {id, title, tasks} = request.body
+  const { id, title, tasks } = request.body
 
   const project = {
     id,
@@ -29,19 +27,44 @@ server.post('/projects', (request, response) => {
 
   projects.push(project)
 
-  return response.json(project)
+  return response.json(projects)
+})
+
+server.get('/projects', (request, response) => {
+  return response.json(projects)
 })
 
 server.put('/projects/:id', (request, response) => {
-  const {id} = request.params
-  const {title, tasks} = request.body
+  const { id } = request.params
+  const { title, tasks } = request.body
 
   const identifyProject = projects.find(params => params.id == id)
 
   identifyProject.title = title
   identifyProject.tasks = tasks
 
-  return response.json(identifyProject)
+  return response.json(projects)
+})
+
+server.delete('/projects/:id', (request, response) => {
+  const {id} = request.params
+
+  const projectIndex = projects.findIndex(params => params.id == id)
+
+  projects.splice(projectIndex, 1)
+
+  return response.json(projects)
+})
+
+server.post('/projects/:id/tasks', (request, response) => {
+  const {id} = request.params
+  const {title} = request.body
+
+  const addTask = projects.find(params => params.id == id)
+
+  addTask.tasks.push(title)
+
+  return response.json(projects)
 })
 
 server.listen(3000)
