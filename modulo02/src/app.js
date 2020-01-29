921a2ec36ad46ea9087cfe2b4f210f1a7b5e4ba9
victorgcampanella <vgcgalvao@outlook.com>
2020-01-29 18:35:47 -0300

    melhorando a aplicacao

---
+++ modulo02/src/app.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+import express from 'express'
+import routes from './routes'
+
+class App{
+  constructor(){
+    this.server = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares(){
+    this.server.use(express.json())
+  }
+
+  routes(){
+    this.server.use(routes)
+  }
+}
+
+// module.exports = new App().server
+export default new App().server
