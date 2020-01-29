4b06a97409ffe67e7d5ef434494d06c48f11c471
victorgcampanella <vgcgalvao@outlook.com>
2020-01-29 14:36:46 -0300

    realizando metodo get post e put

--- desafiomodulo1/index.js
+++ desafiomodulo1/index.js
@@ -4,3 +4,44 @@ const server = express()
 
 server.use(express.json())
 
+const projects = [{id : '1', 
+                  title : 'Novo Projeto', 
+                  tasks: ['Fazer código', 'Fazer tela']
+                  },
+                  {id : '2', 
+                  title : 'Novo Prick', 
+                  tasks: ['Fazer botao', 'Fazer cadastro']
+                  },
+                ]
+
+server.get('/projects', (request, response) => {
+  return response.json(projects)
+})
+
+server.post('/projects', (request, response) => {
+  const {id, title, tasks} = request.body
+
+  const project = {
+    id,
+    title,
+    tasks
+  }
+
+  projects.push(project)
+
+  return response.json(project)
+})
+
+server.put('/projects/:id', (request, response) => {
+  const {id} = request.params
+  const {title, tasks} = request.body
+
+  const identifyProject = projects.find(params => params.id == id)
+
+  identifyProject.title = title
+  identifyProject.tasks = tasks
+
+  return response.json(identifyProject)
+})
+
+server.listen(3000)
