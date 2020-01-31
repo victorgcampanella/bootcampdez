72d34e338baa0fb28d4f70060f026e8db120cf0d
victorgcampanella <vgcgalvao@outlook.com>
2020-01-31 12:50:58 -0300

    adicionando bcrypt

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -2,9 +2,17 @@ import User from '../models/User';
 
 class UserController {
   async store(request, response) {
-    const user = await User.create(request.body);
+    const userExists = await User.findOne({
+      where: { email: request.body.email },
+    });
 
-    return response.json(user);
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists' });
+    }
+
+    const { id, name, email, provider } = await User.create(request.body);
+
+    return response.json({ id, name, email, provider });
   }
 }
 
