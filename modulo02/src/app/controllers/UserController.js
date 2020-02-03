c26d6fa569f0c549a328eea1652c26d2b4ee952c
victorgcampanella <vgcgalvao@outlook.com>
2020-02-03 19:05:18 -0300

    adicionando middlewares e update

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -16,6 +16,32 @@ class UserController {
 
     return response.json({ id, name, email, provider });
   }
+
+  async update(request, response) {
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return response.status(400).json({
+          error: 'User already exists',
+        });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(request.body);
+
+    return response.json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -4,6 +4,8 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 // Usar para teste quando nao tiver feito um controller ainda
@@ -18,5 +20,9 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
