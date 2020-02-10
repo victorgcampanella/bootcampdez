dc3b0fdbc93898dd17dec45c79a5082922ab6563
victorgcampanella <vgcgalvao@outlook.com>
2020-02-10 13:14:29 -0300

    criando relacionamento com model users para avatar

--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 // import User from './app/models/User';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Usar para teste quando nao tiver feito um controller ainda
 // routes.get('/', async (request, response) => {
@@ -24,5 +28,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
