72417d387e5d3c69dc83e05a7740a6fa88b3a18d
victorgcampanella <vgcgalvao@outlook.com>
2020-02-11 12:55:57 -0300

    agendamento de servico

--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -7,13 +7,14 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-// Usar para teste quando nao tiver feito um controller ainda
+// Usar para teste quando nao tiver feito um controller
 // routes.get('/', async (request, response) => {
 //   const user = await User.create({
 //     name: 'Victor',
@@ -31,6 +32,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
