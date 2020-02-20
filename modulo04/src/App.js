ce70f87d8863026c3da2128514f7e63c0d9f2c99
victorgcampanella <vgcgalvao@outlook.com>
2020-02-20 19:33:59 -0300

    class component inicio

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
 
-import profile from './assets/profile.png'
+// import profile from './assets/profile.png'
 
 function App() {
-  // return <h1>Hello Rocketseat</h1>
-  return <img src={profile} width="200"/>
+  return <h1>Hello Rocketseat</h1>
+  // return <img src={profile} width="200"/>
 }
 
 export default App
---
+++ modulo04/src/components/TechList.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+class TechList extends Component{
+  render(){
+    return
+  }
+}
