0e65338286823e10e73e970ef2de9677a953a6f9
Victor Campanella <victor.campanella@itst.com.br>
2020-02-20 09:33:00 -0300

    adicionando babel e webpack parte1

---
+++ modulo04/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      
+    ]
+  }
+}
