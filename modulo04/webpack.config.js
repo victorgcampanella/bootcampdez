25b13294f85a3d9ff98125cedf54b084554755a4
victorgcampanella <vgcgalvao@outlook.com>
2020-02-20 19:25:16 -0300

    trabalhando com imagem e comecando class component

--- modulo04/webpack.config.js
+++ modulo04/webpack.config.js
@@ -19,11 +19,17 @@ module.exports = {
         }
       },
       {
-        test: /\.css/,
+        test: /\.css$/,
         use: [
           {loader: 'style-loader'},
           {loader: 'css-loader'}
         ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader'
+        }
       }
     ]
   }
