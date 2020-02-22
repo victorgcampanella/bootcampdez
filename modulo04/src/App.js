66341b8f9fe79f6364f72fda401792c9e88341fd
victorgcampanella <vgcgalvao@outlook.com>
2020-02-22 17:47:02 -0300

    class components

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 
+import TechList from './components/TechList'
 // import profile from './assets/profile.png'
 
 function App() {
-  return <h1>Hello Rocketseat</h1>
+  // return <h1>Hello Rocketseat</h1>
   // return <img src={profile} width="200"/>
+  return <TechList />
 }
 
 export default App
--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -1,7 +1,25 @@
 import React, {Component} from 'react'
 
 class TechList extends Component{
+  state = {
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native'
+    ]
+  }
+
   render(){
-    return
+    console.log(this.state)
+
+    return (
+      <ul>
+        <li>Node.js</li>
+        <li>ReactJS</li>
+        <li>React Native</li>
+      </ul>
+    )
   }
 }
+
+export default TechList
