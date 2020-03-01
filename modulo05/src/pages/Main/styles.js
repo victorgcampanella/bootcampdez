fa18f95fee73c6cd53ac9bc96b265685c398881a
victorgcampanella <vgcgalvao@outlook.com>
2020-03-01 15:29:47 -0300

    styled components parte final

--- modulo05/src/pages/Main/styles.js
+++ modulo05/src/pages/Main/styles.js
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 export const Title = styled.h1`
   font-size: 24px;
-  color: #7159c1;
+  color: ${props => (props.error ? 'red' : '#7159c1')};
   font-family: Arial, Helvetica, sans-serif;
+
+  small {
+    font-size: 14px;
+    color: #333;
+  }
 `;
