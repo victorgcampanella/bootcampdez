5be6308f53f52ace12d18d06b697505e15546274
victorgcampanella <vgcgalvao@outlook.com>
2020-03-08 20:08:14 -0300

    utilizando localstorage

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -12,6 +12,24 @@ export default class Main extends Component {
     loading: false,
   };
 
+  // Carregar os dados do LocalStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salvar os dados do LocalStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
