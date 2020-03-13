30078d0105a27d901e4cd62fc4292bf8b60449df
victorgcampanella <vgcgalvao@outlook.com>
2020-03-13 17:05:34 -0300

    eslint prettier e editor config e começo do native

--- modulo06/App.js
+++ modulo06/App.js
@@ -1,27 +1,23 @@
+/* eslint-disable no-undef */
 import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
-
-import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import { View, Text, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 32,
+    textAlign: 'center',
   },
 });
 
-export default App;
+export default function App() {
+  return (
+    <View style={styles.view}>
+      <Text style={styles.welcome}>Bem vindo ao React Native</Text>
+    </View>
+  );
+}
