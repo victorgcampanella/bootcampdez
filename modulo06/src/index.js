078b01beb4c482b75a23a1ee06395458b5b2f2e7
victorgcampanella <vgcgalvao@outlook.com>
2020-03-15 20:14:53 -0300

    configurando statusbar

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
@@ -6,5 +7,10 @@ import Routes from './routes';
 console.tron.log('Hello Deu certo?');
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
