67c0a2354841b8c9e2b1a6678b65ca1b1cd029a6
victorgcampanella <vgcgalvao@outlook.com>
2020-03-15 20:11:18 -0300

    react navigation

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,38 +1,10 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  welcome: {
-    fontSize: 32,
-    fontStyle: 'italic',
-    textAlign: 'center',
-    backgroundColor: '#5179c1',
-    borderRadius: 4,
-    padding: 20,
-    margin: 50,
-    maxWidth: 350,
-  },
-
-  button: {
-    borderRadius: 50,
-  },
-});
+import Routes from './routes';
 
 console.tron.log('Hello Deu certo?');
 
 export default function App() {
-  return (
-    <View style={styles.view}>
-      <Text style={styles.welcome}>Bem vindo ao React Native</Text>
-      <Button style={styles.button} title="Enviar" />
-    </View>
-  );
+  return <Routes />;
 }
