f86f3adca3d6929323900f4526ccc42cc8903fc2
victorgcampanella <vgcgalvao@outlook.com>
2020-03-19 12:51:18 -0300

    listando favoritos

---
+++ modulo06/src/pages/User/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #c1c1c1;
+`;
+
+export const Avatar = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 20px;
+  color: #333;
+  font-weight: bold;
+  margin-top: 10px;
+  text-align: center;
+`;
+
+export const Bio = styled.Text`
+  font-size: 15px;
+  line-height: 22px;
+  color: #999;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const Stars = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const Starred = styled.View`
+  background: #e1e1e1;
+  border-radius: 5px;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const OwnerAvatar = styled.Image`
+  height: 42px;
+  width: 42px;
+  border-radius: 21px;
+  background: #eee;
+`;
+
+export const Info = styled.View`
+  margin-left: 10px;
+  flex: 1;
+`;
+
+export const Title = styled.Text.attrs({
+  numberOfLines: 1,
+})`
+  font-size: 15px;
+  font-weight: bold;
+  color: #333;
+`;
+
+export const Author = styled.Text`
+  font-size: 13px;
+  color: #666;
+  margin-top: 2px;
+`;
