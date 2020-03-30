6dd549137b242e16f887a4be6abab838f3974346
victorgcampanella <vgcgalvao@outlook.com>
2020-03-30 13:06:50 -0300

    configurando redux

--- modulo06/src/config/ReactotronConfig.js
+++ modulo06/src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.3.104' })
+  const tron = Reactotron.configure({ host: '192.168.3.105' })
     .useReactNative()
     .connect();
 
---
+++ modulo07/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "linebreak-style": 0,
+    "react/jsx-filename-extension": [
+      "warn",
+      {extensions: [".jsx", ".js"]}
+    ],
+    "import/prefer-default-export": "off",
+    "react/state-in-constructor": "off",
+    "react/static-property-placement": "off",
+  },
+};
