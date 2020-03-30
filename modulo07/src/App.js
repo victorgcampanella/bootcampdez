4a8463ff50f49586d226dc6d54f7d3347e9bb750
victorgcampanella <vgcgalvao@outlook.com>
2020-03-30 17:51:17 -0300

    estruturando projeto para redux

--- modulo07/src/App.js
+++ modulo07/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Redux</h1>
-    </div>
-  );
+  return <h1>Hello Redux</h1>;
 }
 
 export default App;
